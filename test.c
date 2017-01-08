0931eaf541eb9267a42716bc2e3b35177dd177ad
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2017-01-08 23:43:17 +0000

    add tests

--- test.c
+++ test.c
@@ -6,18 +6,23 @@
 /*   By: qle-guen <qle-guen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/26 16:03:07 by qle-guen          #+#    #+#             */
-/*   Updated: 2016/12/27 17:27:38 by qle-guen         ###   ########.fr       */
+/*   Updated: 2017/01/08 23:40:55 by qle-guen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libfmt/libfmt.h"
+#include <limits.h>
 
 int		main(void)
 {
-	long a;
-
-	a = -10;
-	ECHO("%-16", a);
-
+	ECHO("0: %-10/4", 0);
+	ECHO("-10: %-10/4", -10);
+	ECHO("INT_MIN: %-10/4", INT_MIN);
+	ECHO("INT_MAX: %-10/4", INT_MAX);
+	ECHO("LONG_MIN: %-10/8", LONG_MIN);
+	ECHO("LONG_MAX: %-10/8", LONG_MAX);
+	ECHO("0: %10/4", 0);
+	ECHO("UINT_MAX: %10/4", UINT_MAX);
+	ECHO("ULONG_MAX: %10/4", ULONG_MAX);
 	return (0);
 }
